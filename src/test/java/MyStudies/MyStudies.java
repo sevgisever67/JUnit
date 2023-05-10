16938a60c891bd893aa4928aa34ef6cd196581f6
sevgi67 <elizafirs0@gmail.com>
2023-05-10 08:40:20 +0300

    10/05/23

--- src/test/java/MyStudies/MyStudies.java
+++ src/test/java/MyStudies/MyStudies.java
@@ -1,4 +1,4 @@
-package day06_JUnitFramework;
+package MyStudies;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
--- src/test/java/day06_JUnitFramework/C05_Assertions.java
+++ src/test/java/day06_JUnitFramework/C05_Assertions.java
@@ -2,7 +2,9 @@ package day06_JUnitFramework;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 public class C05_Assertions {
@@ -19,21 +21,38 @@ public class C05_Assertions {
         driver= new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        driver.get("https://www.bestbuy.com");
+        driver.get("https://www.bestbuy.com/");
     }
     @Test
     public void test01(){
         // ○ Sayfa URL’inin https://www.bestbuy.com/ ‘a esit oldugunu test edin
         mahserin4Atlisi();
-        String expectedUrl="https://www.bestbuy.com";
+        String expectedUrl="https://www.bestbuy.com/";
         String actualUrl= driver.getCurrentUrl(); // https://www.bestbuy.com/
         Assert.assertEquals(expectedUrl,actualUrl);
     }
     @Test
     public void test02(){
         // ○ titleTest => Sayfa başlığının “Rest” içermediğini(contains) test edin
+        mahserin4Atlisi();
         String unExpectedIcerik = "Rest";
         String actualTitle= driver.getTitle();
         Assert.assertFalse(actualTitle.contains(unExpectedIcerik));
+        driver.close();
+    }
+    @Test
+    public void test03(){
+        mahserin4Atlisi();
+        WebElement logo= driver.findElement(By.xpath("//img[@width='80']"));
+        logo.isDisplayed();
+        driver.close();
+    }
+
+    @Test
+    public void test04(){
+        mahserin4Atlisi();
+        WebElement franchlink= driver.findElement(By.className("is-active"));
+        franchlink.isDisplayed();
+        driver.close();
     }
 }
